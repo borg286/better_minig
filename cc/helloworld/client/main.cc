30fbd0434f2121f1b5f669da30573f2073ff54c8
Brian Matthews <borg286@gmail.com>
2018-12-15 17:13:54 +0000

    playing with cc

--- cc/helloworld/client/main.cc
+++ cc/helloworld/client/main.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include "cc/helloworld/client/simple.h"
 
@@ -22,9 +24,15 @@ int main(int argc, char** argv) {
     std::cerr << "Expected single IP address argument." << std::endl;
     return 1;
   }
-  std::string address(argv[0]);
-  address.append(":");
+  std::string address("dns://");
   address.append(argv[1]);
+  address.append(".default.svc.cluster.local");
+  address.append(":");
+  address.append(argv[2]);
+
+  while (true) {
+
+  std::cout << "Trying to connect to " << address << std::endl;
 
   // Instantiate the client. It requires a channel, out of which the
   // actual RPCs are created. This channel models a connection to an
@@ -34,6 +42,8 @@ int main(int argc, char** argv) {
   std::string user("world");
 
   std::cout << "Foo(" << user << "): " << simple.Foo(user) << std::endl;
+  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+  }
 
   return 0;
 }
